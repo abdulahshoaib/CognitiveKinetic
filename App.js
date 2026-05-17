7de58277b3729644290184ac5902e4b6ee6cd7a6
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2026-05-17 20:53:27 +0500

    chore: finalize app shell safe area wrapping and update ignore rules

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/context/AuthContext';
+import { AnalysisProvider } from './src/context/AnalysisContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <AnalysisProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </AnalysisProvider>
       </AuthProvider>
     </SafeAreaProvider>
   );
 }
-
